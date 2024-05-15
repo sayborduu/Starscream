d43adb94d8533d815ec7d450fb6dde0087794571
SayBored <62549299+sayborduu@users.noreply.github.com>
2024-05-15 13:43:36 +0200

    Update WSCompression.swift

--- Sources/Compression/WSCompression.swift
+++ Sources/Compression/WSCompression.swift
@@ -27,7 +27,7 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
 import Foundation
-import zlib
+import Zlib
 
 public class WSCompression: CompressionHandler {
     let headerWSExtensionName = "Sec-WebSocket-Extensions"
